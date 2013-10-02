7230e55a8d98045b3571fd2dca9c3dedeb609770
Bob Nisco <BobNisco@gmail.com>
2013-10-02 10:46:56 -0400

    Added sides

--- src/Cube.java
+++ src/Cube.java
@@ -41,6 +41,13 @@ public class Cube {
 	 */
 	public static Map<Character, int[]> FACES = initFaces();
 
+	/**
+	 * A Map<Character, int[]>, where the key is the character of the
+	 * color of the face and the int[] is the indices of the string in
+	 * which the side represents.
+	 */
+	public static Map<Character, int[]> SIDES = initSides();
+
     /**
      * Initializes the state to an empty String.
      * Initializes the corners and edges with the positions in our serialized
@@ -194,6 +201,90 @@ public class Cube {
 		face[7] = 53;
 		faces.put("W".charAt(0), face);
 		return faces;
+	}
+
+	private static Map<Character, int[]> initSides {
+		Map<Character, int[]> sides = new HashMap<Character, int[]>();
+		int[] side = new int[12];
+		side[0] = 9;
+		side[1] = 10;
+		side[2] = 11;
+		side[3] = 12;
+		side[4] = 13;
+		side[5] = 14;
+		side[6] = 15;
+		side[7] = 16;
+		side[8] = 17;
+		side[9] = 51;
+		side[10] = 52;
+		side[11] = 53;
+		sides.put("R".charAt(0), side);
+		side[0] = 0;
+		side[1] = 3;
+		side[2] = 6;
+		side[3] = 12;
+		side[4] = 21;
+		side[5] = 30;
+		side[6] = 36;
+		side[7] = 39;
+		side[8] = 42;
+		side[9] = 45;
+		side[10] = 48;
+		side[11] = 51;
+		sides.put("G".charAt(0), side);
+		side[0] = 6;
+		side[1] = 7;
+		side[2] = 8;
+		side[3] = 11;
+		side[4] = 15;
+		side[5] = 20;
+		side[6] = 24;
+		side[7] = 29;
+		side[8] = 33;
+		side[9] = 36;
+		side[10] = 37;
+		side[11] = 38;
+		sides.put("Y".charAt(0), side);
+		side[0] = 2;
+		side[1] = 5;
+		side[2] = 8;
+		side[3] = 14;
+		side[4] = 23;
+		side[5] = 32;
+		side[6] = 41;
+		side[7] = 44;
+		side[8] = 47;
+		side[9] = 50;
+		side[10] = 53;
+		side[11] = 54;
+		sides.put("B".charAt(0), side);
+		side[0] = 27;
+		side[1] = 28;
+		side[2] = 29;
+		side[3] = 30;
+		side[4] = 31;
+		side[5] = 32;
+		side[6] = 33;
+		side[7] = 34;
+		side[8] = 35;
+		side[9] = 45;
+		side[10] = 46;
+		side[11] = 47;
+		sides.put("O".charAt(0), side);
+		side[0] = 0;
+		side[1] = 1;
+		side[2] = 2;
+		side[3] = 9;
+		side[4] = 17;
+		side[5] = 18;
+		side[6] = 26;
+		side[7] = 27;
+		side[8] = 35;
+		side[9] = 42;
+		side[10] = 43;
+		side[11] = 44;
+		sides.put("W".charAt(0), side);
+		return sides;
 	}
 
     /**
